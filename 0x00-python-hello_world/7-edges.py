38a40cc682c1e98e9d49ada8048d25ae0a4854b3
Christian <ch.ov@outlook.com>
2022-05-02 21:44:56 +0000

    7

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+word = "Holberton"
+word_first_3 = word_last_2 = middle_word = word
+print(f"First 3 letters: {word_first_3[:3]}")
+print(f"Last 2 letters: {word_last_2[-2:]}")
+print(f"Middle word: {middle_word[1:-1]}")
