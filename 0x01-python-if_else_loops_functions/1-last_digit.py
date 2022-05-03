9d899edadec8e1210959db292fa7dfeb076c1396
Christian <ch.ov@outlook.com>
2022-05-03 17:27:57 +0000

    1

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 import random
-number = -10
+number = random.randint(-10000, 10000)
 if number <= 0:
     if number * -1 % 10 == 0:
         print(f"Last digit of {number} is -{number % 10} and is 0")
