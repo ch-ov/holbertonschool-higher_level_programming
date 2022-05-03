f3e18a6f27ec89312555c8c6d721bdb8ff7777be
Christian <ch.ov@outlook.com>
2022-05-03 19:46:52 +0000

    5-print_comb2.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -2,7 +2,5 @@
 for y in range(0, 100):
     if y == 99:
         print(y)
-    if y < 10:
-        print(f"0{y}, ", end="")
-    elif y >= 10:
-        print(f"{y}, ", end="")
+    else:
+        print(f"{y:02d}, ", end="")
