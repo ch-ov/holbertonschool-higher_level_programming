cee780c4d5605623061e8b4e51fb644b9e9d1082
Christian <ch.ov@outlook.com>
2022-05-04 04:43:36 +0000

    13

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -8,31 +8,25 @@
  */
 listint_t *insert_node(listint_t **head, int number)
 {
-	/* 1. allocate node */
 	listint_t *new_node = (listint_t *)malloc(sizeof(listint_t));
+	listint_t *last = *head;
 
-	listint_t *last = *head; /* used in step 5*/
-
-	/* 2. put in the data  */
+	new_node->n = number;
+	if (new_node == NULL)
+		return (NULL);
 	new_node->n = number;
 
-	/* 3. This new node is going to be the last node, so make next of it as NULL*/
-	new_node->next = NULL;
-
-	/* 4. If the Linked List is empty, then make the new node as head */
-	if (new_node == NULL || new_node->n >= number)
+	if (last == NULL || last->n >= number)
 	{
-		new_node->next = new_node;
+		new_node->next = last;
 		*head = new_node;
 		return (new_node);
 	}
-
-	/* 5. Else traverse till the last node */
-	while (new_node && new_node->next && new_node->next->n < number)
+	while (last && last->next && last->next->n < number)
 		last = last->next;
 
-	/* 6. Change the next of last node */
-	new_node->next = new_node->next;
+	new_node->next = last->next;
 	last->next = new_node;
+
 	return (new_node);
 }
