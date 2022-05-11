7892fcb1824114e984c4f99a550046334faf0851
Christian <ch.ov@outlook.com>
2022-05-11 02:25:51 +0000

    12

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+roman = {}
+roman['I'] = 1
+roman['V'] = 5
+roman['X'] = 10
+roman['L'] = 50
+roman['C'] = 100
+roman['D'] = 500
+roman['M'] = 1000
+
+
+def roman_to_int(roman_string):
+    sum = 0
+    n = len(roman_string)
+
+    i = 0
+    while i < n:
+
+        if (i != n-1 and roman[roman_string[i]] < roman[roman_string[i + 1]]):
+            sum += roman[roman_string[i + 1]] - roman[roman_string[i]]
+            i += 2
+            continue
+        else:
+            sum += roman[roman_string[i]]
+        i += 1
+    return sum
