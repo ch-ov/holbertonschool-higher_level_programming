7ba4e51b7bf681391a6b473d4b60325b8880862d
Christian <ch.ov@outlook.com>
2022-05-11 03:19:12 +0000

    12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,28 +1,21 @@
 #!/usr/bin/python3
-roman = {}
-roman['I'] = 1
-roman['V'] = 5
-roman['X'] = 10
-roman['L'] = 50
-roman['C'] = 100
-roman['D'] = 500
-roman['M'] = 1000
-
-
 def roman_to_int(roman_string):
-    sum = 0
-    n = len(roman_string)
 
-    i = 0
-    while i < n:
+    roman = {}
+    roman['I'] = 1
+    roman['V'] = 5
+    roman['X'] = 10
+    roman['L'] = 50
+    roman['C'] = 100
+    roman['D'] = 500
+    roman['M'] = 1000
+    if not isinstance(roman_string, str) or roman_string is None:
+        return 0
+    result = 0
 
-        if not isinstance(roman_string, str) or roman_string is None:
-            return (0)
-        if (i != n-1 and roman[roman_string[i]] < roman[roman_string[i + 1]]):
-            sum += roman[roman_string[i + 1]] - roman[roman_string[i]]
-            i += 2
-            continue
+    for i, c in enumerate(roman_string):
+        if (i+1) == len(roman_string) or roman[c] >= roman[roman_string[i+1]]:
+            result += roman[c]
         else:
-            sum += roman[roman_string[i]]
-        i += 1
-    return (sum)
+            result -= roman[c]
+    return result
