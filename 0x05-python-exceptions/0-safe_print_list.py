2b6981583e25a584a1096f17b06df11b5387e727
Christian <ch.ov@outlook.com>
2022-05-23 21:10:38 +0000

    0-safe_print_list.py

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    try:
+        y = 0
+        for i in range(x):
+            print(my_list[i], end="")
+            y += 1
+        print("")
+        return x
+    except IndexError:
+        print("")
+        return y
