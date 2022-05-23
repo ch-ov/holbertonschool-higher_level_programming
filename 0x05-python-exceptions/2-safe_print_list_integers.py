b6e9abae99c13baa6ada4309a8ccf884f1447817
Christian <ch.ov@outlook.com>
2022-05-23 21:46:49 +0000

    2-safe_print_list_integers.py

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -10,6 +10,6 @@ def safe_print_list_integers(my_list=[], x=0):
                 continue
         print("")
         return (y)
-    except (ValueError, IndexError):
+    except (ValueError):
         print("")
         return (y)
