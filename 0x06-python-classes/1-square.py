b34d01202b16ce8864f7f61b40b8fe7b29e7c7ea
Christian <ch.ov@outlook.com>
2022-05-24 22:49:11 +0000

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Class Square that defines a square"""
+
+
+class Square:
+    """A class with Private instance attribute"""
+    def __init__(self, size):
+        self.__size = size
