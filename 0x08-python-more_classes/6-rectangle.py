e238d11bf3e1f4518a4870c96a66b212cdb058dc
Christian <ch.ov@outlook.com>
2022-06-01 02:55:28 +0000

    6-rectangle.py

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+""" class Rectangle that defines a rectangle by: (based on 5-rectangle.py) """
+
+
 class Rectangle:
+    """ How many instances """
     number_of_instances = 0
 
     def __init__(self, width=0, height=0):
