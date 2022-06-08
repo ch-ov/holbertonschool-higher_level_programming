020384542a2278e54fde3a77ad7ec899226f1805
Christian <ch.ov@outlook.com>
2022-06-08 02:32:11 +0000

    else

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -7,3 +7,4 @@ def inherits_from(obj, a_class):
     (directly or indirectly) from the specified class ; otherwise False"""
     if (type(obj) != a_class):
         return issubclass(type(obj), a_class)
+    return False
