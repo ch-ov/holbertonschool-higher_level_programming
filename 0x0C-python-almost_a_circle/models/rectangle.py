ca8f01e61c24ebc2e8e61059e8b4a00a95ebd43c
Christian <ch.ov@outlook.com>
2022-06-15 00:16:52 +0000

    rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
+"""2. First Rectangle"""
 
 
 from models.base import Base
-"""2. First Rectangle"""
+
 
 class Rectangle(Base):
     """class Rectangle that inherits from Base"""
