11871dd7b2641ac88d5f1e3c26f2bb27a29a6c0f
Christian <ch.ov@outlook.com>
2022-06-15 00:16:30 +0000

    rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -4,7 +4,6 @@
 from models.base import Base
 """2. First Rectangle"""
 
-
 class Rectangle(Base):
     """class Rectangle that inherits from Base"""
     def __init__(self, width, height, x=0, y=0, id=None):
