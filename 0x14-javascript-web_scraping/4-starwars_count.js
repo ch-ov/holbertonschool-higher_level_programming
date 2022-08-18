6b4e4917462887d82a3ccead2a7e3b1e7b54c09e
christian <ch.ov@outlook.com>
2022-08-18 03:00:56 +0000

    4 && 5

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+const request = require('request');
+request(process.argv[2], function (error, response, body) {
+  if (error) console.log(error);
+  else if (response.statusCode) {
+    const films = JSON.parse(body).results;
+    let count = 0;
+    for (const film in films) {
+      const characters = films[film].characters;
+      for (const char in characters) {
+        if (characters[char].includes('18')) {
+          count++;
+        }
+      }
+    }
+    console.log(count);
+  }
+});
