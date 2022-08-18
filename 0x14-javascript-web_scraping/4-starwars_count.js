379daddd60b9e1099f9654e797b23adc4a3f8508
christian <ch.ov@outlook.com>
2022-08-18 03:11:24 +0000

    4-starwars_count.js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const request = require('request');
 request(process.argv[2], function (err, response, body) {
-  if (err) console.log(error);
+  if (err) console.log(err);
   else if (response.statusCode) {
     const films = JSON.parse(body).results;
     let count = 0;
