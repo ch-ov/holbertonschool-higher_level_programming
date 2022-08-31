0ae51e97b8dc146093a7b18bc5820d12f557ee6a
christian <ch.ov@outlook.com>
2022-08-31 00:59:05 +0000

    model_state.py

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -5,7 +5,7 @@ from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
-
+Base.metadata.create_all(engine)
 
 class State(Base):
     __tablename__ = "states"
