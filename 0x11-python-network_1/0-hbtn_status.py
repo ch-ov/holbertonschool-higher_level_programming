018627585a99155a51774aa55243534274a09a0c
christian <ch.ov@outlook.com>
2022-09-02 23:46:14 +0000

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""fetches https://intranet.hbtn.io/status"""
+"""Write a Python script that fetches https://intranet.hbtn.io/status"""
 
 import urllib.request
 
@@ -7,9 +7,8 @@ import urllib.request
 if __name__ == "__main__":
 
     with urllib.request.urlopen("https://intranet.hbtn.io/status") as response:
-        if response.readable():
-            data = response.read()
-            print("Body response:")
-            print(f"\t- type: {type(data)}")
-            print(f"\t- content: {data}")
-            print(f"\t- utf8 content: {data.decode('utf-8')}")
+        data = response.read()
+        print("Body response:")
+        print(f"\t- type: {type(data)}")
+        print(f"\t- content: {data}")
+        print(f"\t- utf8 content: {data.decode('utf-8')}")
